8a9fb20233375b86440a90e73af1a67e8a60c7ef
“red.i” <red.i@inbox.lv>
2020-11-16 15:52:05 +0200

    Second commit

--- src/main/java/redi/sbdemoapp/spring5webapp/Spring5webappApplication.java
+++ src/main/java/redi/sbdemoapp/spring5webapp/Spring5webappApplication.java
@@ -9,6 +9,7 @@ public class Spring5webappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
 		System.out.println("Say HI!");
+		System.out.println("Say HI=HI!");
 	}
 
 }
